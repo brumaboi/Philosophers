099fb328a2e6b6a2349789e392fe49528602cefc
brumaboi <brumastefan8@gmail.com>
2024-09-30 12:17:04 +0200

    started project, prep

---
+++ inc/philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 11:57:09 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/30 11:57:09 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+////// STRUCTURES //////
+
+
+////// FUNCTIONS //////
+
+
+
+#endif

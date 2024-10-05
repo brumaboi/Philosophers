c12b99d838373519cc20fb836bb0ba0f6c70cfad
brumaboi <brumastefan8@gmail.com>
2024-10-05 12:18:47 +0200

    created eat function(no race, updating vars), also created function that checks if philo is not dead/going to die and can get the forks

--- src/philo.c
+++ src/philo.c
@@ -48,12 +48,54 @@ int one_dead(t_data *data)
 
 void ft_sleep(t_philo *philo, t_data *data)
 {
-    ////
+    print_action(data, philo, "is sleeping");
+    ft_usleep(data->time_to_sleep);
+}
+
+int give_forks_not_dead(t_philo *philo, t_data *data)
+{
+    lock_forks(philo, data);
+    pthread_mutex_lock(&philo->mutex);
+    will_starve(philo, data);
+    if(one_dead(data) == 1)
+    {
+        pthread_mutex_unlock(&philo->mutex);
+        unlock_forks(philo, data);
+        return (1);
+    }
+    pthread_mutex_unlock(&philo->mutex);
+    return (0);
+}
+
+lock_forks(t_philo *philo, t_data *data)
+{
+    
+}
+
+unlock_forks(t_philo *philo, t_data *data)
+{
+
 }
 
 void ft_eat(t_philo *philo, t_data *data)
 {
-    ////
+    if (give_forks_not_dead(philo, data) == 1)
+        return ;
+    print_action(data, philo, "is eating");
+    pthread_mutex_lock(&philo->mutex);
+    philo->last_meal = ft_get_time();
+    pthread_mutex_unlock(&philo->mutex);
+    ft_usleep(data->time_to_eat);
+    pthread_mutex_lock(&philo->mutex);
+    philo->meals++;
+    pthread_mutex_unlock(&philo->mutex);
+    if (data->meal_count != -1 && philo->meals >= data->meal_count)
+    {
+        pthread_mutex_lock(&data->full_mutex);
+        data->full++;
+        pthread_mutex_unlock(&data->full_mutex);
+    }
+    unlock_forks(philo, data);
 }
 
 void *routine(void *arg)
@@ -65,7 +107,7 @@ void *routine(void *arg)
     data =  philo->data;
     if (philo->id % 2 == 0)
         ft_usleep(data->time_to_eat / 2);
-    while(one_dead(data == 1))
+    while(one_dead(data) == 1)
     {
         print_action(data, philo, "is thinking");
         ft_eat(philo, data);
